be2ab2862ac918fc4241c445020eeaba0f2ec8f6
Emmanuel Noufele <noufelee@yahoo.com>
2020-04-04 17:54:36 -0500

    Change location, graduation date, gender and pronouns to select and add all their attributes

--- src/routes/profile/index.svelte
+++ src/routes/profile/index.svelte
@@ -33,20 +33,6 @@
     margin-bottom: 7px
 }
 
-.profile-sidebar-portlet {
-    padding: 30px 0 0!important
-}
-
-.profile-sidebar {
-    float: left;
-    width: 300px;
-    margin-right: 20px
-}
-
-.profile-content {
-    overflow: hidden
-}
-
 .profile-userbuttons {
     text-align: center;
     margin-top: 10px
@@ -66,8 +52,42 @@
     font-weight: 600;
     padding: 6px 15px
 }
-input[type=text], select {
+ select[id = "state"] {
   width: 100%;
+  height: 7%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  border: 2px solid #67bef4;
+}
+ input[type=date] {
+  width: 100%;
+  height: 7%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  border: 2px solid #67bef4;
+}
+ input[type=text] {
+  width: 100%;
+  height: 7%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  border: 2px solid #67bef4;
+}
+ select[id = "gender"] {
+  width: 100%;
+  height: 7%;
   padding: 12px 20px;
   margin: 8px 0;
   display: inline-block;
@@ -89,7 +109,7 @@ input[type=submit] {
 
 textarea[type=text], select {
   width: 100%;
-  height: 20%;
+  height: 15%;
   padding: 12px 20px;
   margin: 8px 0;
   display: inline-block;
@@ -107,23 +127,21 @@ b1{
     width: 420px;
     height: 420px;
     padding: 25px;
-    /* border: 2px solid #67bef4;
-    background-color: #f2f2f2; */
     margin: 0;
 }
-.right {
-  width: 25%;
-}
+
 h2{
     font-size: 350%;
 }
-  p{
-    border-radius: 5px;
-    background-color: #f2f2f2;
-    padding: 20px;
+  button {
+      background-color:  #67bef4;
+      border: 2px solid #67bef4;
   }
   pr{
     color: #67bef4;
+  }
+  fn {
+      color: #546975;
   }
 </style>
 
@@ -136,12 +154,12 @@ h2{
         <h2>Profile</h2>
     </pr>
     <div class="row">
-        <b1 id="right">
+        <b1>
             <div class="profile-userpic">
                 <img src="https://bootdey.com/img/Content/avatar/avatar6.png" class="img-responsive d-flex justify-content-center" alt="">
             </div>
             <div class="profile-usertitle">
-                <div class="profile-usertitle-name"> First Name Last Name </div>
+                <fn class="profile-usertitle-name"> First Name Last Name </fn>
                 <div class="profile-usertitle-job"> Profession </div>
             </div>
             <div class="profile-userbuttons">
@@ -151,14 +169,87 @@ h2{
         </b1>
         <div class="col-md-6 d-flex justify-content-center"> 
             <form >
-                <label for="ulocation">Location</label>                    
-                <input type="text" id = "ulocation" name="location" placeholder=" Your location.."/>
+                <label for="state">Location</label>                    
+                <select id = "state" name="location">
+                    <option value="" selected="selected">Select your location</option>
+                    <option value="AL">Alabama</option>
+                    <option value="AK">Alaska</option>
+                    <option value="AZ">Arizona</option>
+                    <option value="AR">Arkansas</option>
+                    <option value="CA">California</option>
+                    <option value="CO">Colorado</option>
+                    <option value="CT">Connecticut</option>
+                    <option value="DE">Delaware</option>
+                    <option value="DC">District Of Columbia</option>
+                    <option value="FL">Florida</option>
+                    <option value="GA">Georgia</option>
+                    <option value="HI">Hawaii</option>
+                    <option value="ID">Idaho</option>
+                    <option value="IL">Illinois</option>
+                    <option value="IN">Indiana</option>
+                    <option value="IA">Iowa</option>
+                    <option value="KS">Kansas</option>
+                    <option value="KY">Kentucky</option>
+                    <option value="LA">Louisiana</option>
+                    <option value="ME">Maine</option>
+                    <option value="MD">Maryland</option>
+                    <option value="MA">Massachusetts</option>
+                    <option value="MI">Michigan</option>
+                    <option value="MN">Minnesota</option>
+                    <option value="MS">Mississippi</option>
+                    <option value="MO">Missouri</option>
+                    <option value="MT">Montana</option>
+                    <option value="NE">Nebraska</option>
+                    <option value="NV">Nevada</option>
+                    <option value="NH">New Hampshire</option>
+                    <option value="NJ">New Jersey</option>
+                    <option value="NM">New Mexico</option>
+                    <option value="NY">New York</option>
+                    <option value="NC">North Carolina</option>
+                    <option value="ND">North Dakota</option>
+                    <option value="OH">Ohio</option>
+                    <option value="OK">Oklahoma</option>
+                    <option value="OR">Oregon</option>
+                    <option value="PA">Pennsylvania</option>
+                    <option value="RI">Rhode Island</option>
+                    <option value="SC">South Carolina</option>
+                    <option value="SD">South Dakota</option>
+                    <option value="TN">Tennessee</option>
+                    <option value="TX">Texas</option>
+                    <option value="UT">Utah</option>
+                    <option value="VT">Vermont</option>
+                    <option value="VA">Virginia</option>
+                    <option value="WA">Washington</option>
+                    <option value="WV">West Virginia</option>
+                    <option value="WI">Wisconsin</option>
+                    <option value="WY">Wyoming</option>
+                </select>
+                
+                <label for="school">School Name</label>  
+                <input type="text" id="school" name="Enter your school here..">
 
-                <label for="school">School & Grad Year</label>   
-                <input type="text" id = "school" name="school-grad-year" placeholder=" Your school & grad year.."/>
+                <label for="graduation">Graduation Date</label>  
+                <input type="date" id="graduation" name="Your grad date.."> 
 
                 <label for="gender">Gender & Pronouns</label>   
-                <input type="text" id = "gender" name="gender-pronouns" placeholder=" Your Gender & Pronouns.."/>
+                <select id = "gender" name="gender-pronouns">
+                    <option value="" selected="selected">Select Your Gender</option>
+                    <option value="M">Male</option>
+                    <option value="F">Female</option>
+                    <option value="B">Bigender</option>
+                    <option value="A">Agender</option>
+                    <option value="O">Other</option>
+                </select>
+                <select id = "gender" name="gender-pronouns">
+                    <option value="" selected="selected">Select Your Pronouns</option>
+                    <option value="He">He</option>
+                    <option value="She">She</option>
+                    <option value="It">It</option>
+                    <option value="They">They</option>
+                    <option value="His">His</option>
+                    <option value="Her">Her</option>
+                    <option value="Its">Its</option>
+                </select>
                 
                 <label for="bio">Bio</label>
                 <textarea type="text" placeholder="Enter your bio here"/>
